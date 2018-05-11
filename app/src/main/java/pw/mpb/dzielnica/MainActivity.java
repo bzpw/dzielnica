05a226ed093283b70d755e9f5adc45dd59f6ca41
bzpw <bzaniewski.gin@gmail.com>
2018-05-12 00:13:27 +0200

    Ekrany testowe. Bazowy ekran logowania. Update.

---
+++ app/src/main/java/pw/mpb/dzielnica/MainActivity.java
@@ -0,0 +1,27 @@
+package pw.mpb.dzielnica;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button yourButton = (Button)findViewById(R.id.button2);
+
+        yourButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, user_login.class));
+            }
+        });
+    }
+
+
+
+}
